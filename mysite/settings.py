6259bd49a80a8bb0ad09ea0e2e97cd05ff32e83e
Farhan Mohammed Mansoor <farhan.mansoordxb@gmail.com>
2025-04-19 20:00:39 +0400

    Render Deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-+6dw9gw8kvw=n2t7g2hew@m0j*+&_bc7zmj&p1&8u(4)p85a2n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '.onrender.com']
 
 # Application definition
 
